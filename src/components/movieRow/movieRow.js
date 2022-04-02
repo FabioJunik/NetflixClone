c33d64babd30ee3b336305f7678275f60e988f23
Fábio Junik <fabiojunik@gmail.com>
2022-04-02 11:43:02 +0100

    Estilo basico da lista

--- src/components/movieRow/movieRow.js
+++ src/components/movieRow/movieRow.js
@@ -3,12 +3,17 @@ import './movieRow.css';
 
 const MovieRow = ({title, items} ) =>{
     return(
-        <div >
+        <div className="movieRow">
             <h2>{title}</h2>
             <div className='movieRow_listArea'>
-                {items.results.length > 0 && items.results.map((item, key)=>(
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt="capa"/>
-                ))}
+                <div className="movieRow_list">
+                    {items.results.length > 0 && items.results.map((item, key)=>(
+                        <div className="movieRow_item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                        </div>
+                    ))}
+                </div>
+                
             </div>
         </div>
    )
