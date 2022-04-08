805d11770292469f8aa0f7d89a7ada19f5cbadb5
Fábio Junik <fabiojunik@gmail.com>
2022-04-08 09:00:05 +0100

    mostrando as informações do filme/serie

--- src/components/featureMovie/featureMovie.js
+++ src/components/featureMovie/featureMovie.js
@@ -2,9 +2,39 @@ import React from "react";
 import './featureMovie.css'
 
 const FeatureMovie = ({item})=>{
+    const firstDate = new Date(item.first_air_date);
+    let genres = [];
+    
+    for(let i in item.genres){
+        genres.push(item.genres[i].name);
+    }
+
+    // const genres = item.genres.map(genre=> {console.log(genre)});
+    // console.log(genres);
     return(
-        <section className='featured' style={{backgroundImage:`url(http://image.tmdb.org/t/p/original${item.backdrop_path})`}}>
-            <div>{item.original_name} {console.log(item.backdrop_path)}</div>
+        <section className='featured' style={{backgroundImage:`url(http://image.tmdb.org/t/p/original${item.backdrop_path})`}} >
+            <div className="featuredVertical">
+                <div className="featuredHorizontal">
+                    <h1>{item.original_name}</h1>
+                    <div className="info">
+                        <span>{item.vote_average} pontos</span>
+                        <span>{firstDate.getFullYear()}</span>
+                        <span>{item.number_of_seasons} temporada{item.number_of_seasons != 1?'s':''}</span>
+                    </div>
+                    <div className="featuredDescripition">
+                        {item.overview}
+                    </div>
+                    <div className="featuredButton">
+                        <a href={`/watch/${item.id}`}> Assistir</a>
+                        <a href={`/list/add/${item.id}`}>+ Minha lista</a>
+                    </div>
+                    <div className="featuredGenres">
+                        <strong>Generos: </strong>
+                        {genres.join(', ')}
+                    </div>
+                    {console.log(item)}
+                </div>
+            </div>
         </section>
     );
 }
